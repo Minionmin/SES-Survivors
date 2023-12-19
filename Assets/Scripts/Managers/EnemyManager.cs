9317e52d31f343cb03f09ff944c1e392e5452f1d
Min <funnymin555@gmail.com>
2023-12-19 19:40:30 +0900

    敵のリストを実装

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public List<Enemy> enemyList;
+
+    private void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
