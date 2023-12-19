16b913a0cd1dbf54cc0e2a1fdb9553eb78b71c01
Min <funnymin555@gmail.com>
2023-12-19 18:03:50 +0900

    プレイヤーのビジュアル+基本インプット（ムーブメント）

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -5,21 +5,28 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    /// <summary> インプット処理クラス </summary>
     private InputControl inputControl;
 
+    /// <summary> プレイヤーの移動方向 </summary>
     private Vector2 moveDir;
 
     private void Awake()
     {
+        // インプット処理クラスを生成
         inputControl = new InputControl();
+
+        // インプット処理クラスを有効化
         inputControl.Player.Enable();
     }
 
     private void Update()
     {
+        // キーボード・スティックからインプット値を読む
         moveDir = inputControl.Player.Move.ReadValue<Vector2>();
     }
 
+    /// <summary> プレイヤーの移動方向Vector3をゲット </summary>
     public Vector3 GetMoveDirection()
     {
         return new Vector3(moveDir.x, 0.0f, moveDir.y).normalized;
